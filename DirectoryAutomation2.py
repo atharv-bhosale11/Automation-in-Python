603f3b6e7f68f94da8929e396a90ab913f686460
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-07-28 18:08:57 +0530

    Add Python script for directory scanning with file size reporting   Imported os and sys modules for filesystem operations.   Implemented DirectoryScanner to validate path existence and type.   Handled error cases for missing or invalid directories.   Traversed directory tree using os.walk and printed filenames.   Fixed path issue by joining folder and filename before calling os.path.getsize.   Added command-line argument validation and user guidance in main function.

---
+++ DirectoryAutomation2.py
@@ -0,0 +1,36 @@
+import os
+import sys
+
+def DirectoryScanner(DirName="Marvellous"):
+    Ret = False
+
+    Ret = os.path.exists(DirName)
+
+    if Ret==False:
+        print("There is No such Directory")
+        return
+    
+    Ret = os.path.isdir(DirName)
+
+    if Ret == False:
+        print("It is not directory")
+        return
+
+    for FolderName,SubFolder,FileName in os.walk(DirName):
+        for fname in FileName:
+            print("File Name: ",fname)
+            print("File Size: ",os.path.getsize(fname))     #path issue so error
+def main():
+    Border = "-"*52
+    print(Border)
+    print("---------------Marveelous Automation----------------")
+    print(Border)
+
+    if(len(sys.argv)!=2):
+        print("Invalid number of arguments")
+        print("Please!! Specify the Name of Directoru")
+        return
+
+    DirectoryScanner(sys.argv[1])
+if __name__ == "__main__":
+    main()
