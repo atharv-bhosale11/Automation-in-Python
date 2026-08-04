02b045d96e4598d183924d78ebe852aee57921bd
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-08-04 18:45:17 +0530

    Add Python script to demonstrate seek operation from current offset   Implemented main function to open Hello.txt in read mode.   Printed initial file offset using fobj.tell().   Used fobj.seek(6,1) to move pointer 6 bytes forward from current position.   Read 6 characters from the new offset and displayed them.   Printed updated offsets before and after reading for clarity.   Handled FileNotFoundError gracefully and ensured final message with finally block.

---
+++ FileIO1ExceptionReadSeekXX.py
@@ -0,0 +1,35 @@
+#Seek operation from back
+
+#seek(from where, to where)
+#kuthun : 0/1/2
+#0 : Starting
+#1 : Current
+#2 : End
+
+def main():
+    try:
+        fobj = open("Hello.txt","r")
+        print("File gets successfully opend!!!!!")
+
+        print("Current Offset is: ",fobj.tell())    #0
+        
+        fobj.seek(6,1)
+
+        print("Current Offset is: ",fobj.tell())    #11
+
+        Data = fobj.read(6)
+        
+        print("Current Offset is: ",fobj.tell())    #17
+
+        print("Data from the file is: ",Data)
+        
+        fobj.close()
+        
+    except FileNotFoundError:
+        print("Unable to open file as there is no such file")
+
+    finally:
+        print("End of Application")
+
+if __name__=="__main__":
+    main()
