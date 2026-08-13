33a95aca2c59c307f6e3bcf6eab1385606cedd7f
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-08-13 09:07:09 +0530

    Add Python program to check file existence and open in read mode   Displayed file object capabilities: readable, writable, and seekable

---
+++ FileIO1FunctionsX.py
@@ -0,0 +1,17 @@
+import os
+
+def main():
+    FileName = input("Enter tha Name of File: ")
+
+    if os.path.exists(FileName):
+        fobj = open(FileName,"r")
+
+        print(fobj.readable())  #True
+        print(fobj.writable())  #False
+        print(fobj.seekable())  #True
+
+    else:
+        print("There is no such file")
+        
+if __name__=="__main__":
+    main()
